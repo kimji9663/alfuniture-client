b5b4228ef5cc4807cfb294024100f63737427b38
Jihye <kimji9663@nate.com>
2023-07-10 18:53:36 +0900

    초기 세팅

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+const NavigationBar = () => {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/auth/login">Login</Link>
+    </div>
+  )
+}
+
+export default NavigationBar
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import NavigationBar from "../components/NavigationBar"
+
+const Home = () => {
+  return (
+    <>
+      <div>홈</div>
+      <NavigationBar/>
+    </>
+  )
+}
+
+export default Home
