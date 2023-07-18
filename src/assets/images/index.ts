cbd7ab2a699f585359dabe8fa3ca0d8a529bbff3
kimji9663 <rlawlgp9663@naver.com>
2023-07-19 00:15:01 +0900

    assets 디렉토리 추가, 이미지 추가

---
+++ src/assets/images/index.ts
@@ -0,0 +1,13 @@
+import icon_home from "./icon_home.png"
+import icon_style from "./icon_style.png"
+import icon_shop from "./icon_shop.png"
+import icon_like from "./icon_like.png"
+import icon_my from "./icon_my.png"
+
+export {
+    icon_home,
+    icon_style,
+    icon_shop,
+    icon_like,
+    icon_my,
+}
--- src/components/navigationbar.styles.ts
+++ src/components/navigationbar.styles.ts
@@ -3,9 +3,21 @@ import { styled } from "@mui/material/styles"
 import { Box } from "@mui/material"
 
 export const NavWrap = styled(Box)(() => ({
+  display: 'flex',
+  alignItems: 'center',
   position: 'absolute',
   bottom: 0,
   width: '100%',
   height: '74px',
   backgroundColor: '#000',
+
+  '& > a': {
+    flex: '1 1 20%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    textAlign: 'center',
+    color: '#fff',
+  }
 }))
+
