d4547c15ddb1cf20592d1300d56652b3e51cf5df
kimji9663 <rlawlgp9663@naver.com>
2023-07-19 23:21:27 +0900

    Tabs -> NormalTabs 로 이름 변경, 프리티어 세팅

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,12 +1,77 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { Tabs, Tab, Typography, Box } from "@mui/material"
 
-const NavigationBar = () => {
+interface TabPanelProps {
+  children?: React.ReactNode
+  index: number
+  value: number
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
   return (
-    <div>
-      탭 컴포넌트
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
     </div>
   )
 }
 
-export default NavigationBar
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  }
+}
+
+const NormalTabs = () => {
+  const [value, setValue] = React.useState(0)
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue)
+  }
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ 
+        borderBottom: 1, 
+        borderColor: 'divider',
+        '.MuiTab-root.Mui-selected': { color: '#333' },
+        '.MuiTabs-indicator': { height: '1px', backgroundColor: '#333' },
+      }}>
+        <Tabs 
+          value={value}
+          variant="fullWidth" 
+          onChange={handleChange} 
+          aria-label="basic tabs example"
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        Item One
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        Item Two
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        Item Three
+      </CustomTabPanel>
+    </Box>
+  )
+}
+
+export default NormalTabs
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { StyledBox, exampleStyle } from "./home.styles"
+import NormalTabs from "../components/Tabs"
 import NavigationBar from "../components/NavigationBar"
 
 const Home = () => {
   return (
     <>
       <StyledBox>홈</StyledBox>
+      <NormalTabs />
       <NavigationBar/>
     </>
   )
