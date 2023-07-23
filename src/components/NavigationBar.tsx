965b8f66685db5ad54868d483d6a723e05b819f6
psk910903 <104986080+psk910903@users.noreply.github.com>
2023-07-23 16:58:05 +0900

    submit

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,5 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavigationBar = () => {
   return (
@@ -7,7 +7,7 @@ const NavigationBar = () => {
       <Link to="/">Home</Link>
       <Link to="/auth/login">Login</Link>
     </div>
-  )
-}
+  );
+};
 
-export default NavigationBar
+export default NavigationBar;
