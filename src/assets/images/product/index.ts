20c4d2213557d00450ad068934f41ca0d70ede6d
kimji9663 <rlawlgp9663@naver.com>
2023-08-09 16:07:01 +0900

    동적 코드로 수정

--- src/assets/images/product/index.ts
+++ src/assets/images/product/index.ts
@@ -2,9 +2,39 @@
 import sofa01 from "./sofa01.png"
 import sofa02 from "./sofa02.png"
 import sofa03 from "./sofa03.png"
+import table01 from "./table01.png"
+import table02 from "./table02.png"
+import table03 from "./table03.png"
+import chair01 from "./chair01.png"
+import chair02 from "./chair02.png"
+import chair03 from "./chair03.png"
+import closet01 from "./closet01.png"
+import closet02 from "./closet02.png"
+import closet03 from "./closet03.png"
+import bed01 from "./bed01.png"
+import bed02 from "./bed02.png"
+import bed03 from "./bed03.png"
+import light01 from "./light01.png"
+import light02 from "./light02.png"
+import light03 from "./light03.png"
 
 export {
     sofa01,
     sofa02,
     sofa03,
+    table01,
+    table02,
+    table03,
+    chair01,
+    chair02,
+    chair03,
+    closet01,
+    closet02,
+    closet03,  
+    bed01,
+    bed02,
+    bed03,
+    light01,
+    light02,
+    light03,
 }
