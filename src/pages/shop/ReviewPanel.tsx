f22906378cd09ea8dea6bb475b9e720679a156b6
kimji9663 <rlawlgp9663@naver.com>
2023-09-07 17:56:29 +0900

    상품상세 리뷰 구현

--- src/pages/shop/ReviewPanel.tsx
+++ src/pages/shop/ReviewPanel.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react"
-import { Box, Divider } from "@mui/material"
-import { ViewTitle, AverageGrade, DetailGrade, PhotoReview } from "./productView.styles"
-import { IconStar } from "../../assets/images"
+import { Box, Divider, Rating } from "@mui/material"
+import StarIcon from "@mui/icons-material/Star"
 import { Swiper, SwiperSlide } from "swiper/react"
 import "swiper/css"
-import { sofa01 } from "../../assets/images/product"
+import { ViewTitle, AverageGrade, DetailGrade, PhotoReviewWrap, ReviewerInfo, UserPhotoWrap, ProductOptions } from "./productView.styles"
+import { sofa01, sofa02, light01, light02, chair01, chair02, table01, table02 } from "../../assets/images/product"
 
 type ISwiperProps = { 
   loop?: boolean
@@ -14,145 +14,221 @@ type ISwiperProps = {
     el: null 
   }
   slidesPerView?: "auto" | number | undefined
+  onSlideChange?: (swiper: any) => void
 }
 
-const ReviewPanel = () => {
+const photoReviewList = [
+  {
+    id: 1,
+    img: light01,
+    discription: '조명01'
+  },
+  {
+    id: 2,
+    img: chair01,
+    discription: '의자01'
+  },
+  {
+    id: 3,
+    img: sofa01,
+    discription: '소파01'
+  },
+  {
+    id: 4,
+    img: table01,
+    discription: '테이블01'
+  },
+  {
+    id: 5,
+    img: light02,
+    discription: '조명02'
+  },
+  {
+    id: 6,
+    img: chair02,
+    discription: '의자02'
+  },
+  {
+    id: 7,
+    img: sofa02,
+    discription: '소파02'
+  },
+  {
+    id: 8,
+    img: table02,
+    discription: '테이블02'
+  }
+]
 
-  const settings:ISwiperProps = {
-    loop: true,
+const userPhotoList = [
+  {
+    id: 1,
+    img: light01,
+    discription: '조명01'
+  },
+  {
+    id: 2,
+    img: chair01,
+    discription: '의자01'
+  },
+  {
+    id: 3,
+    img: sofa01,
+    discription: '소파01'
+  },
+  {
+    id: 4,
+    img: sofa02,
+    discription: '소파02'
+  }
+]
+
+const ReviewPanel = () => {
+  const [activeUserPhoto, setActiveUserPhoto] = useState(1)
+
+  const photoReviewSettings:ISwiperProps = {
+    loop: false,
     scrollbar: { draggable: true, el: null },
     slidesPerView: 3.3,
     spaceBetween: 8,
   }
 
+  const userPhotoSettings:ISwiperProps = {
+    onSlideChange: (s:any) => {
+      setActiveUserPhoto(s.realIndex + 1)
+    }
+  }
+
   return (
-    <>
-      <Box sx={{ p: 2 }}>
-        <ViewTitle component="p">
-          구매 후기
-          <span>(182)</span>
-        </ViewTitle>
+    <Box sx={{ p: 2, mb: 7 }}>
+      <ViewTitle component="p">
+        구매 후기
+        <span>(182)</span>
+      </ViewTitle>
 
-        <Box sx={{ display: 'flex', '& > .MuiBox-root': { flex: '1 1 50%' } }}>
-          <AverageGrade>
-            <div>
-              <span className="now">4.0</span>
-              <span className="all">/5</span>
-            </div>
-            <div className="grade_star">
-              <IconStar />
-              <IconStar />
-              <IconStar />
-              <IconStar />
-              <IconStar />
-            </div>
-          </AverageGrade>
+      <Box sx={{ display: 'flex', '& > .MuiBox-root': { flex: '1 1 50%' } }}>
+        <AverageGrade>
+          <div>
+            <span className="now">4.0</span>
+            <span className="all">/5</span>
+          </div>
+          <div className="grade_star">
+            <Rating
+              name="text-feedback"
+              value={4}
+              readOnly
+              precision={0.5}
+              emptyIcon={<StarIcon style={{ color: '#eee' }} fontSize="inherit" />}
+            />
+          </div>
+        </AverageGrade>
 
-          <DetailGrade sx={{ ml: 3 }}>
-            <Box>
-              <Box component="span" className="grade_number">5점</Box>
-              <Box component="span" className="grade_pipe">
-                <span style={{ width: '80%' }}></span>
-              </Box>
-            </Box>
-            <Box>
-              <Box component="span" className="grade_number">4점</Box>
-              <Box component="span" className="grade_pipe">
-                <span style={{ width: '60%' }}></span>
-              </Box>
-            </Box>
-            <Box>
-              <Box component="span" className="grade_number">3점</Box>
-              <Box component="span" className="grade_pipe">
-                <span style={{ width: '50%' }}></span>
-              </Box>
-            </Box>
-            <Box>
-              <Box component="span" className="grade_number">2점</Box>
-              <Box component="span" className="grade_pipe">
-                <span style={{ width: '20%' }}></span>
-              </Box>
-            </Box>
-            <Box>
-              <Box component="span" className="grade_number">1점</Box>
-              <Box component="span" className="grade_pipe">
-                <span style={{ width: '10%' }}></span>
-              </Box>
-            </Box>
-          </DetailGrade>
-        </Box>
-
-        <Divider sx={{ my: 2 }} />
-        
-        <ViewTitle component="p">
-          포토 후기
-          <span>(41)</span>
-        </ViewTitle>
-        <PhotoReview>
-          <Swiper {...settings}>
-            <SwiperSlide>
-              <img src={sofa01} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={sofa01} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={sofa01} alt="" />
-            </SwiperSlide>
-            <SwiperSlide>
-              <img src={sofa01} alt="" />
-            </SwiperSlide>
-          </Swiper>
-        </PhotoReview>
-
-        <Divider sx={{ mt: 5 }} />
-
-        <Box sx={{ mt: 5 }}>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <span>va***</span>
-            <span>23.01.28</span>
-          </Box>
-          <Box sx={{ mt: 1, color: '#242223' }}>
-            <IconStar />
-            <IconStar />
-            <IconStar />
-            <IconStar />
-            <IconStar />
-          </Box>
-          <Box>
-            <Box sx={{ mt: 2, position: 'relative', '& img': { width: '100%' } }}>
-              <Swiper>
-                <SwiperSlide>
-                  <img src={sofa01} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                  <img src={sofa01} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                  <img src={sofa01} alt="" />
-                </SwiperSlide>
-                <SwiperSlide>
-                  <img src={sofa01} alt="" />
-                </SwiperSlide>
-              </Swiper>
-              <Box 
-                sx={{ 
-                  position: 'absolute', 
-                  top: 16, 
-                  right: 12, 
-                  zIndex: 1, 
-                  padding: '0 8px',
-                  backgroundColor: '#000', 
-                  color: '#fff',
-                }}
-              >
-                1/3
-              </Box>
-            </Box>
-          </Box>
-        </Box>
+        <DetailGrade sx={{ ml: 3 }}>
+          <div>
+            <span className="grade_number">5점</span>
+            <span className="grade_pipe">
+              <Box component='span' sx={{ width: '80%' }}></Box>
+            </span>
+          </div>
+          <div>
+            <span className="grade_number">4점</span>
+            <span className="grade_pipe">
+              <Box component='span' sx={{ width: '60%' }}></Box>
+            </span>
+          </div>
+          <div>
+            <span className="grade_number">3점</span>
+            <span className="grade_pipe">
+              <Box component='span' sx={{ width: '50%' }}></Box>
+            </span>
+          </div>
+          <div>
+            <span className="grade_number">2점</span>
+            <span className="grade_pipe">
+              <Box component='span' sx={{ width: '20%' }}></Box>
+            </span>
+          </div>
+          <div>
+            <span className="grade_number">1점</span>
+            <span className="grade_pipe">
+              <Box component='span' sx={{ width: '10%' }}></Box>
+            </span>
+          </div>
+        </DetailGrade>
       </Box>
-    </>
+      
+      <ViewTitle component="p" sx={{ mt: 5 }}>
+        포토 후기
+        <span>(41)</span>
+      </ViewTitle>
+      <PhotoReviewWrap>
+        <Swiper {...photoReviewSettings}>
+          {photoReviewList.map((photoReview) => (
+            <SwiperSlide key={`photo_review_${photoReview.id}`}>
+              <img src={photoReview.img} alt={photoReview.discription} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </PhotoReviewWrap>
+
+      <Divider sx={{ mt: 5 }} />
+
+      <ReviewerInfo sx={{ mt: 5 }}>
+        <Box className="nickname_date">
+          <span className="nickname">va***</span>
+          <span className="date">23.01.28</span>
+        </Box>
+        <Box className="grade_star" sx={{ mt: 1 }}>
+          <Rating
+            name="text-feedback"
+            value={4}
+            readOnly
+            precision={0.5}
+            emptyIcon={<StarIcon style={{ color: '#eee' }} fontSize="inherit" />}
+          />
+        </Box>
+      </ReviewerInfo>
+
+      <UserPhotoWrap sx={{ mt: 2 }}>
+        <Swiper {...userPhotoSettings}>
+          {userPhotoList.map((userPhoto) => (
+            <SwiperSlide key={`user_photo_${userPhoto.id}`}>
+              <img src={userPhoto.img} alt="" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        <div className="pagination">
+          {activeUserPhoto}/{userPhotoList.length}
+        </div>
+      </UserPhotoWrap>
+
+      <ProductOptions sx={{ mt: 5 }}>
+        <Box sx={{ display: 'flex' }}>
+          <div className="title">선택옵션</div>
+          <div className="options">
+            <span>아이보리</span>
+            <span>134*60</span>
+          </div>
+        </Box>
+        <Box sx={{ display: 'flex', mt: 2 }}>
+          <div className="title">해쉬태그</div>
+          <div className="hash_tags">
+            <span>생각보다 커요</span>
+            <span>색감 예뻐요</span>
+            <span>가벼워요</span>
+            <span>인스타감성</span>
+            <span>북유럽</span>
+            <span>분위기 있어요</span>
+          </div>
+        </Box>
+      </ProductOptions>
+
+      <Box sx={{ mt: 3, color: '#999', fontSize: '.875rem' }}>
+        한 달 걸려서 받았네요. 아이보리 할까 브라운 할까 고민 많이 했는데 아이보리 하길 너무 잘한 것 같아요. 기다린 보람이 있습니다. 
+        사진에서 보시다 시피 전체 인테리어와 조화를 잘 이루고 또 적당히 포인트가 되는게 정말로 취향저격입니다. 
+        조금 힙하면서도 단정한  분위기를 선호한다면 저는 강추드립니다 :)
+      </Box>
+    </Box>
   )
 }
 
--- src/pages/shop/productView.styles.ts
+++ src/pages/shop/productView.styles.ts
@@ -166,13 +166,20 @@ export const AverageGrade = styled(Box)(() => ({
     color: '#E4E4E4',
     fontSize: '2rem',
   },
-  '& .grade_star > svg': {
-    color: '#EEEEEE'
+  '& .grade_star': {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  '& .grade_star .MuiSvgIcon-root': {
+    width: '18px', 
+    height: '18px', 
+    color: '#F95029'
   }
 }))
 
 export const DetailGrade = styled(Box)(() => ({
-  '& > .MuiBox-root': {
+  '& > div': {
     display: 'flex',
     alignItems: 'center',
     marginBottom: '4px',
@@ -198,7 +205,7 @@ export const DetailGrade = styled(Box)(() => ({
   },
 }))
 
-export const PhotoReview = styled(Box)(() => ({
+export const PhotoReviewWrap = styled(Box)(() => ({
   marginTop: '16px',
   
   '& .swiper-slide': {
@@ -209,4 +216,83 @@ export const PhotoReview = styled(Box)(() => ({
   '& .swiper-slide > img': {
     width: '100%',
   },
+}))
+
+export const ReviewerInfo = styled(Box)(() => ({
+  '& .nickname_date': {
+    display: 'flex', 
+    alignItems: 'center', 
+    justifyContent: 'space-between',
+  },
+  '& .nickname_date .nickname': {
+    color: '#000',
+    fontSize: '.875rem', 
+    fontWeight: 'bold',
+  },
+  '& .nickname_date .date': {
+    color: '#000',
+    fontSize: '.75rem',
+  },
+  '& .grade_star': {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  '& .grade_star .MuiSvgIcon-root': {
+    width: '14px', 
+    height: '14px', 
+    color: '#242223'
+  }
+}))
+
+export const UserPhotoWrap = styled(Box)(() => ({
+  position: 'relative', 
+
+  '& .swiper-wrapper': {
+    height: 'calc(100vw - 32px)',
+  },
+  '& img': { 
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    width: '100%',
+    transform: 'translate(-50%, -50%)',
+  },
+  '& .pagination': {
+    position: 'absolute', 
+    top: 16, 
+    right: 12, 
+    zIndex: 1, 
+    padding: '0 8px',
+    backgroundColor: '#000', 
+    color: '#fff',
+  }
+}))
+
+export const ProductOptions = styled(Box)(() => ({
+  '& .title': {
+    flex: '0 0 66px', 
+    fontSize: '.75rem', 
+    color: '#666', 
+    fontWeight: 'bold',
+  },
+  '& .options': {
+    display: 'flex', 
+  },
+  '& .options > span': {
+    marginRight: '20px',
+    color: '#BDBDBD', 
+    fontSize: '.75rem',
+  },
+  '& .hash_tags': {
+    display: 'flex', 
+    flexWrap: 'wrap', 
+    color: '#999', 
+    fontSize: '.75rem', 
+  },
+  '& .hash_tags > span': { 
+    marginBottom: '8px', 
+    marginRight: '8px',
+    padding: '2px 4px', 
+    border: '1px solid #BDBDBD', 
+  }
 }))
