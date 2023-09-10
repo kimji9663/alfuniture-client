9667642cdcecb128ad662738ab5a4988c882ad9d
psk910903 <104986080+psk910903@users.noreply.github.com>
2023-09-10 21:58:08 +0900

    장바구니 화면

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -2,42 +2,66 @@ import React, { useState, useEffect  } from "react"
 import CenterTitle from "../../components/title/CenterTitle"
 import { sofa01, table01 } from "../../assets/images/product"
 import NoData from "../../components/NoData"
+import ListItem from "./ListItem";
 
-export const data = [
-  {
-    imgsrc: sofa01,
-    like: true,
-    likeCount: 556,
-    shopName: 'One Rain',
-    modelName: '[3Color] OR-205',
-    productName: '원레인 4인용 소파',
-    price: 3230000,
-    colors: ['#1B464D', '#192552', '#4414CD']
-  },
-  {
-    imgsrc: table01,
-    like: false,
-    likeCount: 556,
-    shopName: 'ALFDEN',
-    modelName: '[23Series] AD-388',
-    productName: '알프든 옐로우소파',
-    price: 1430000,
-    colors: ['#BC862E']
-  },
-]
+interface cartData {
+  imgsrc: any,
+  shopName: string,
+  modelName: string,
+  productName: string,
+  option1: string,
+  option2: string,
+  price: Number,
+  deliveryCharge: Number,
+  coupon: Array<object>,
+}
+
 
 const Cart = () => {
-  const [data, setData] = useState(null);
-
+  const [data, setData] = useState<cartData[]>([]);
   const message = "장바구니에 담은 상품이 없습니다.";
   const linkText = "상품 보러가기";
   const linkTo = "/shop";
+
+  const testData: never[] = [
+    // {
+    //   imgsrc: sofa01,
+    //   shopName: "ALFDEN",
+    //   modelName: "[23Series] AD-388",
+    //   productName: "카멜피아",
+    //   option1: "베이지",
+    //   option2: "라탄",
+    //   price: 240000,
+    //   deliveryCharge: 100000,
+    //   coupon: [{
+    //     name: "쿠폰1",
+    //     discount: 14000
+    //   }],
+    // },
+    // {
+    //   imgsrc: table01,
+    //   shopName: "ALFDEN",
+    //   modelName: "[23Series] AD-388",
+    //   productName: "모닝 데스크",
+    //   option1: "옐로우 우든",
+    //   option2: "",
+    //   price: 300000,
+    //   deliveryCharge: 0,
+    //   coupon: [],
+    // },
+  ]
+  useEffect(() => {
+    setData([...data, ...testData]);
+  }, []); 
+
   return (
     <>
       <CenterTitle title={["장바구니"]} />
-      {data ? (
+      {data.length > 0 ? (
         // 데이터가 있을 때 렌더링
-        <div>데이터가 있을 때 렌더링</div>
+        data.map((item, index) => (
+          <ListItem key={index} item={item} isLastItem={index === data.length - 1}/>
+        ))
       ) : (
         // 데이터가 없을 때 렌더링
         <NoData  message={message} linkText={linkText} linkTo={linkTo}/>
