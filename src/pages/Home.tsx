9f0c9ba4d5b4e5f4eec77baa07603cb700c2b3b2
kimji9663 <rlawlgp9663@naver.com>
2023-09-11 11:43:40 +0900

    emotion, mui 스타일링 예시

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,19 @@
+/** @jsxImportSource @emotion/react */
 import React from "react"
+import { ExampleStyle1, ExampleStyle2, exampleStyle } from "./home.styles"
 
 const Home = () => {
   return (
     <>
-      홈
+      <div css={exampleStyle}>
+        홈
+      </div>
+      <ExampleStyle1>
+        홈
+      </ExampleStyle1>
+      <ExampleStyle2>
+        홈
+      </ExampleStyle2>
     </>
   )
 }
