06b7cf57efb2f35bc88f3f4dd41c36df5bd528ed
aaH6uos <107555762+aaH6uos@users.noreply.github.com>
2023-10-15 13:49:52 +0900

    온보딩 스타일 선택

--- src/pages/onboarding/StyleSelection.tsx
+++ src/pages/onboarding/StyleSelection.tsx
@@ -4,9 +4,16 @@ import { NaviWrap } from "../../components/navigationbar.styles"
 import { PrimaryButton } from "../../styles/buttons.styles"
 import { useNavigate } from "react-router-dom"
 import TitleTwoLine2 from '../../components/title/TitleTwoLine2'
+import { GridBox } from "../../components/Box"
+import { StyleTypes } from "../shop/Filters/filters"
+import { StyleCard, StyleCardSelected } from "./StyleSelection.styles"
+import { IconLikeOn } from "../../assets/images"
 
 const StyleSelection = () => {
   const navigate = useNavigate()
+  const [selected, setSelected] = React.useState<any>(
+    StyleTypes.reduce((obj, style) => ({ ...obj, [style.name]: false }), {})
+  );
 
   const goToNext = () => {
     navigate("/onboarding/featured_product")
@@ -14,19 +21,52 @@ const StyleSelection = () => {
 
   const title = ['좋아하는 스타일을', '선택해주세요.'];
 
-    return (
-      <>
-        <Box sx={{ height: '100%', overflow: 'auto' }}>
-          <TitleTwoLine2 title={title}/>
-          {/* <Box sx={{ px: 2, display: 'flex', alignItems: 'center', height: 'calc(100vh - 202px)' }}> */}
-          <Box sx={{ px: 2, display: 'flex', alignItems: 'center' }}>
-            스타일리스트
-          </Box>
-        </Box>
-        <NaviWrap className="single">
-          <PrimaryButton onClick={goToNext}>다음으로</PrimaryButton>
-        </NaviWrap>
-      </>
-    )
+  const handleClickStyle = (styleName: any) => (event: React.MouseEvent) => {
+    setSelected({ ...selected, [styleName]: !selected[styleName] })
   }
-  export default StyleSelection
+
+  return (
+    <>
+      <Box sx={{ height: '100%', overflow: 'auto' }}>
+        <TitleTwoLine2 title={title} />
+        <GridBox
+          gridTemplateColumns={'1fr 1fr'}
+          columnGap={'20px'}
+          rowGap={'17px'}
+          sx={{ mb: '30px', mx: '20px' }}
+        >
+          {StyleTypes.map((style: any) => (
+            <>
+              {selected[style.name] ?
+                <StyleCardSelected
+                  key={`style-card-${style.name}`}
+                  onClick={handleClickStyle(style.name)}
+                  sx={{ backgroundImage: `url(${style.avatar})` }}
+                >
+                  <Box className='title'>
+                  <IconLikeOn />
+                  <Typography>
+                    # {style.name}
+                  </Typography>
+                  </Box>
+                </StyleCardSelected> : <StyleCard
+                  key={`style-card-${style.name}`}
+                  onClick={handleClickStyle(style.name)}
+                  sx={{ backgroundImage: `url(${style.avatar})` }}
+                >
+                  <Typography>
+                    # {style.name}
+                  </Typography>
+                </StyleCard>
+              }
+            </>
+          ))}
+        </GridBox>
+      </Box>
+      <NaviWrap className="single">
+        <PrimaryButton onClick={goToNext}>다음으로</PrimaryButton>
+      </NaviWrap>
+    </>
+  )
+}
+export default StyleSelection
