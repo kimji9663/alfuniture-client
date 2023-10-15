987806c66dd709592d0542aeb107cc4f1df29d7e
Jihye Kim <rlawlgp9663@naver.com>
2023-10-15 21:18:34 +0900

    merge

--- src/pages/onboarding/StyleSelection.tsx
+++ src/pages/onboarding/StyleSelection.tsx
@@ -4,6 +4,7 @@ import { NaviWrap } from "../../components/navigationbar.styles"
 import { PrimaryButton } from "../../styles/buttons.styles"
 import { useNavigate } from "react-router-dom"
 import TitleTwoLine2 from '../../components/title/TitleTwoLine2'
+import BrandListItem from "../../components/product/BrandListItem"
 
 const StyleSelection = () => {
   const navigate = useNavigate()
@@ -20,7 +21,6 @@ const StyleSelection = () => {
           <TitleTwoLine2 title={title}/>
           {/* <Box sx={{ px: 2, display: 'flex', alignItems: 'center', height: 'calc(100vh - 202px)' }}> */}
           <Box sx={{ px: 2, display: 'flex', alignItems: 'center' }}>
-            스타일리스트
           </Box>
         </Box>
         <NaviWrap className="single">
