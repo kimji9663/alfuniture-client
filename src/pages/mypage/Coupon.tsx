1c7fe2518d9fcfe962295b825d86defdfd063684
ssunkyo <psk910903@gmail.com>
2023-10-18 15:53:00 +0900

    쿠폰 정렬 기능 구현

--- src/pages/mypage/Coupon.tsx
+++ src/pages/mypage/Coupon.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react"
-import { Box, Tabs, Tab, Typography } from "@mui/material"
+import React, { useState, useEffect  } from "react"
+import { Box, Tabs, Tab, Typography, Button } from "@mui/material"
 import NavigationBar from "../../components/NavigationBar"
 import CenterTitle from "../../components/title/CenterTitle"
 import { HalfArrowDownGray } from "../../assets/images"
@@ -17,22 +17,22 @@ const data = [
     imgsrc: onerain_coupon03,
     shopName: 'Aerobiey',
     couponName: '에어로우뷔 브랜드 10% 쿠폰',
-    startDate: '20230918',
-    endDate: '20231118',
+    startDate: '20230925',
+    endDate: '20231120',
   },
   {
     imgsrc: onerain_coupon03,
     shopName: 'James Lee',
     couponName: '제임스 리  브랜드 10% 쿠폰',
-    startDate: '20230918',
-    endDate: '20231118',
+    startDate: '20230920',
+    endDate: '20231115',
   },
   {
     imgsrc: onerain_coupon03,
     shopName: 'One rain',
     couponName: '원레인  브랜드 10% 쿠폰',
-    startDate: '20230918',
-    endDate: '20231118',
+    startDate: '20230922',
+    endDate: '20231128',
   },
 ]
 
@@ -41,7 +41,7 @@ const data2 = [
     imgsrc: onerain_coupon03,
     shopName: 'James Lee',
     couponName: '제임스 리  브랜드 10% 쿠폰',
-    startDate: '20230918',
+    startDate: '20230920',
     endDate: '20231118',
   },
   {
@@ -89,6 +89,15 @@ const formattedData2 = data2.map((item) => ({
   dDay: calculateDDay(item.endDate),
 }));
 
+type CouponData = {
+  imgsrc: string;
+  shopName: string;
+  couponName: string;
+  startDate: string;
+  endDate: string;
+  dDay: number;
+};
+
 function couponProps(index: number) {
   return {
     id: `coupon-tab-${index}`,
@@ -118,12 +127,25 @@ function CouponTabPanel(props: TabPanelProps) {
 
 const title = ['쿠폰'];
 
-const Coupon = () => {
+const Coupon: React.FC = () => {
   const [tabValue, setTabValue] = useState(0)
 
   const handleCouponTab = (event:React.SyntheticEvent, newValue: number) => {
     setTabValue(newValue)
   }
+
+  const [data, setData] = useState<CouponData[]>([]); 
+  const [sortedData, setSortedData] = useState<CouponData[]>([]); 
+  const [isSorted, setIsSorted] = useState(false);
+
+  useEffect(() => {
+    setData(formattedData);
+  }, []);
+
+  const sortData = (unsortedData: CouponData[]) => {
+    const sortedDataCopy = [...unsortedData].sort((a, b) => new Date(a.startDate).getTime() - new Date(b.startDate).getTime());
+    return sortedDataCopy;
+  };
 
   return (
     <>
@@ -151,13 +173,28 @@ const Coupon = () => {
             <Box sx={{display:"flex", alignItems:"center", justifyContent:"space-between", mb:"21px"}}>
               <Typography sx={{fontSize: "12px", lineHeight: "12px", letterSpacing: "0.4px", color:"#333333", fontWeight:"500"}}>{"쿠폰 "+formattedData.length+"개"}</Typography>
               <Box sx={{display:"flex"}}>
-                <Typography sx={{fontSize: "12px", lineHeight: "16px", color:"#999999", fontWeight:"400"}}>최신순</Typography>
-                <HalfArrowDownGray style={{paddingTop:2, paddingLeft:2}}/>
+                <Button sx={{color:"#333", display:"flex"}} onClick={() => {
+                  if (!isSorted) {
+                    const sortedDataCopy = sortData(formattedData);
+                    setSortedData(sortedDataCopy);
+                    setIsSorted(true);
+                  } else {
+                    setIsSorted(false);
+                  }
+                }}>
+                  <Typography sx={{fontSize: "12px", lineHeight: "16px", color:"#999999", fontWeight:"400"}}>최신순</Typography>
+                  <HalfArrowDownGray style={{paddingTop:2, paddingLeft:4}}/>
+                </Button>
               </Box>  
             </Box>
             
             {/* 쿠폰 */}
-            <CouponCard data={formattedData} hasCoupon={true}/>
+            
+            {isSorted ? (
+              <CouponCard data={sortedData} hasCoupon={true}/>
+            ) : (
+              <CouponCard data={formattedData} hasCoupon={true}/>
+            )}
           </Box>
         </CouponTabPanel>
 
@@ -166,13 +203,27 @@ const Coupon = () => {
             <Box sx={{display:"flex", alignItems:"center", justifyContent:"space-between", mb:"21px"}}>
               <Typography sx={{fontSize: "12px", lineHeight: "12px", letterSpacing: "0.4px", color:"#333333", fontWeight:"500"}}>{"쿠폰 "+formattedData2.length+"개"}</Typography>
               <Box sx={{display:"flex"}}>
+              <Button sx={{color:"#333", display:"flex"}} onClick={() => {
+                if (!isSorted) {
+                  const sortedDataCopy = sortData(formattedData2);
+                  setSortedData(sortedDataCopy);
+                  setIsSorted(true);
+                } else {
+                  setIsSorted(false);
+                }
+              }}>
                 <Typography sx={{fontSize: "12px", lineHeight: "16px", color:"#999999", fontWeight:"400"}}>최신순</Typography>
-                <HalfArrowDownGray style={{paddingTop:2, paddingLeft:2}}/>
+                <HalfArrowDownGray style={{paddingTop:2, paddingLeft:4}}/>
+              </Button>
               </Box>  
             </Box>
             
             {/* 쿠폰 */}
-            <CouponCard data={formattedData2} hasCoupon={false}/>
+            {isSorted ? (
+              <CouponCard data={sortedData} hasCoupon={false}/>
+            ) : (
+              <CouponCard data={formattedData2} hasCoupon={false}/>
+            )}
           </Box>
         </CouponTabPanel>
       </Box>
