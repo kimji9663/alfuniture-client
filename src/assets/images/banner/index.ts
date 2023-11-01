fd8b5fc0b41dfbdece412d38f209339ed6b018df
ssunkyo <psk910903@gmail.com>
2023-11-01 10:39:40 +0900

    이미지명  snake_case로 수정

--- src/assets/images/banner/index.ts
+++ src/assets/images/banner/index.ts
@@ -1,14 +1,14 @@
 // Image
-import styleBanner01 from "./styleBanner01.png"
-import styleBanner02 from "./styleBanner02.png"
-import styleBanner03 from "./styleBanner03.png"
-import styleBanner04 from "./styleBanner04.png"
-import styleBanner05 from "./styleBanner05.png"
-import styleBanner06 from "./styleBanner06.png"
-import styleBanner07 from "./styleBanner07.png"
-import brandCollectionVideo01 from "./brandCollectionVideo01.png"
-import brandCollectionVideo02 from "./brandCollectionVideo02.png"
-import brandCollectionVideo03 from "./brandCollectionVideo03.png"
+import styleBanner01 from "./style_banner01.png"
+import styleBanner02 from "./style_banner02.png"
+import styleBanner03 from "./style_banner03.png"
+import styleBanner04 from "./style_banner04.png"
+import styleBanner05 from "./style_banner05.png"
+import styleBanner06 from "./style_banner06.png"
+import styleBanner07 from "./style_banner07.png"
+import brandCollectionVideo01 from "./brand_collection_video01.png"
+import brandCollectionVideo02 from "./brand_collection_video02.png"
+import brandCollectionVideo03 from "./brand_collection_video03.png"
 
 export {
     styleBanner01,
--- src/pages/home/detailViewPanel.styles.ts
+++ src/pages/home/detailViewPanel.styles.ts
@@ -1,6 +1,5 @@
 import { styled } from "@mui/material/styles"
 import { Box } from "@mui/material"
-import { OutlineButton } from "../../styles/buttons.styles"
 
 export const DetailView = styled(Box)(() => ({
   position: 'relative',
